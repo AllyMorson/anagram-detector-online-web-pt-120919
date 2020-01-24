571ef980152182dd165a306ef524524780d17038
Ally Morson <ally.morson@gmail.com>
2020-01-24 01:59:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+#write a program that, given a word and a list of possible anagrams, selects the correct one(s).
+
+#e,g listen = Anagram.new("listen")
+#listen.match(%w(enlists google inlets banana))
+ 
+class Anagram 
+  attr_accessor :name 
+  
+  def initialize(word)
+    @name = word 
+  end   
+  
+  #match method takes an array of possible anagrams. It should return all matches in an array.
+  
+   def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
